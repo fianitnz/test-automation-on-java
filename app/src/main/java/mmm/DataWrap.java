b4ff76a6f6be3cbd76f43732401af07ba873fbc9
fianitnz <fianitnz@gmail.com>
2021-07-05 05:51:08 +0300

    first commit

---
+++ app/src/main/java/mmm/DataWrap.java
@@ -0,0 +1,22 @@
+package mmm;
+
+// import com.google.gson.annotations.SerializedName;
+
+public class DataWrap {
+    public int status;
+
+    // @SerializedName("col")
+    public int col;
+    // @SerializedName("code")
+    public int code;
+    // @SerializedName("s")
+    public String[] s;
+    // @SerializedName("len")
+    public int len;
+    // @SerializedName("pos")
+    public int pos;
+    // @SerializedName("row")
+    public int row;
+    // @SerializedName("word")
+    public String word;
+}
